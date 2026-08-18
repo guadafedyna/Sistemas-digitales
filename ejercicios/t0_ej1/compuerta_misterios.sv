ac85a3570af050638f730e25e4a76c5f3f26974b
Guadalupe Fedyna <guadafedyna@gmail.com>
2026-08-18 00:37:04 +0000

    comments

--- ejercicios/t0_ej1/compuerta_misterios.sv
+++ ejercicios/t0_ej1/compuerta_misterios.sv
@@ -2,10 +2,10 @@
 // Abrir, sintetizar y explorar.
 
 module compuerta_misterios (
-    input  logic a,
-    output logic y
+    input  logic a,  //como entrada la variable "a" recibe valores 
+    output logic y   //como salida la variable "y" recibe una asignacion en el cuerpo del modulo 
 );
 
-  assign y = ~a;
+  assign y = ~a;  //asigna la relacion logica entre a; y .el operador que usa es NOT. invierte el input
 
 endmodule
